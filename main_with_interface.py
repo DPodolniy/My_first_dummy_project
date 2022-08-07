1d7a504a3c98754ec3ae72502fe303896a00d888
DPodolniy <danilpodolniy@gmail.com>
2022-08-07 18:09:19 +0300

    v. 0.05
    Check ReadMe

--- main_with_interface.py
+++ main_with_interface.py
@@ -1,7 +1,8 @@
+from tkinter import *
 from random import randint, choice
 
 
-def passwordscreen():  # функция защитного экрана
+def passwordscreen():  # 'Защитный' экран
     difficulty = 4
     # difficulty = randint(4,6) ## рандомный выбор сложности в разработке
     symbolpool = ['.', ',', '!', '@', '#', '$', '%', '^', '&', '*', '()']  # 11 символов для визуального шума
@@ -331,172 +332,30 @@ def passwordscreen():  # функция защитного экрана
             passwordpool1[randint(0, 74)] = inserter
             wordpool4.remove(inserter)
         # Вывод визуала
-        print(*passwordpool1)
-        print(*passwordpool2)
-        print(*passwordpool3)
-        print(*passwordpool4)
-        print(*passwordpool5)
-        print(*passwordpool6)
-        print(*passwordpool7)
-        print(*passwordpool8)
-        print(*passwordpool9)
-        print(*passwordpool10)
-        # Тут я остановился в своем ревью
+        lbl1 = Label(window, text = passwordpool1, font = ("Arial Bold", 10))
+        lbl1.grid(column = 0, row = 0)
+        lbl2 = Label(window, text = passwordpool2, font = ("Arial Bold", 10))
+        lbl2.grid(column = 0, row = 1)
+        lbl3 = Label(window, text = passwordpool3, font = ("Arial Bold", 10))
+        lbl3.grid(column = 0, row = 2)
+        lbl4 = Label(window, text = passwordpool4, font = ("Arial Bold", 10))
+        lbl4.grid(column = 0, row = 3)
+        lbl5 = Label(window, text = passwordpool5, font = ("Arial Bold", 10))
+        lbl5.grid(column = 0, row = 4)
+        lbl6 = Label(window, text = passwordpool6, font = ("Arial Bold", 10))
+        lbl6.grid(column = 0, row = 5)
+        lbl7 = Label(window, text = passwordpool7, font = ("Arial Bold", 10))
+        lbl7.grid(column = 0, row = 6)
+        lbl8 = Label(window, text = passwordpool8, font = ("Arial Bold", 10))
+        lbl8.grid(column = 0, row = 7)
+        lbl9 = Label(window, text = passwordpool9, font = ("Arial Bold", 10))
+        lbl9.grid(column = 0, row = 8)
+        lbl10 = Label(window, text = passwordpool10, font = ("Arial Bold", 10))
+        lbl10.grid(column = 0, row = 9)
 
+window = Tk()
+window.title('pythonProject')
+window.geometry('1000x500')
+passwordscreen()
+window.mainloop()
 
-passwordscreen()  # вызываем экран пароля при запуске
-user_try, try_counter = '', 0
-
-# процедура подбора пароля
-while True:
-    user_try = input()  # повтор ввода
-    if user_try == pass_word or user_try == 'skip':
-        print('Успешно!')
-        break
-    try_counter += 1
-    if try_counter > 4 and user_try != pass_word:
-        print('все возможные попытки исчерпаны')
-        print('ТЕРМИНАЛ ЗАБЛОКИРОВАН')
-
-    else:
-        right_char_counter = 0
-        print('Не тот пароль, попробуй снова. Количество попыток ', 5 - try_counter)  # Вывод текста ошибочного пароля
-        for i in range(4):
-            for j in range(i, 4):
-                if user_try[j] == pass_word[i]:
-                    right_char_counter += 1
-        print('Верные символы - ', right_char_counter)
-
-
-# функциональный блок
-def grosstonet(pay):
-    ndfl = 0.13
-    return pay * (1-ndfl)
-
-
-def nettogross(pay):
-    ndfl = 0.13
-    pay = (pay / ((1-ndfl)*100) * 100)
-    return int(pay)
-
-
-def password_generator():
-    def is_valid(s):
-        if s in yes:
-            return 'yeah'
-        elif s in no:
-            return 'nope'
-        else:
-            return False
-
-    def generate_password(length, chars):
-        password = ''
-        for i in range(length):
-            password += chars[randint(0, len(chars) - 1)]
-        return password
-
-    digits, lowercase_letters = '0123456789', 'abcdefghijklmnopqrstuvwxyz'
-    uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    punctuation, chars, yes, no = '!#$%&*+-=?@^_', '', ['Да', 'да', 'ДА'], ['Нет', 'нет', 'Нет']
-    symb7 = ['i', 'L', 'l', '0', 'o', 'O', '1']
-    print('Добро пожаловать в генератор паролей.\n'
-          'Количество паролей для генерации?')
-    inpt1 = input()
-    while True:
-        if inpt1.isdigit() is True:
-            if int(inpt1) > 0:
-                inpt1 = int(inpt1)
-                break
-            else:
-                print('А может быть все-таки введем целое число больше нуля?')
-                inpt1 = input()
-        else:
-            print('А может быть все-таки введем целое число больше нуля?')
-            inpt1 = input()
-    print('Длина генерируемых паролей?')
-    inpt2 = input()
-    while True:
-        if inpt2.isdigit() is True:
-            if int(inpt2) > 0:
-                inpt2 = int(inpt2)
-                break
-            else:
-                print('А может быть все-таки введем целое число больше нуля?')
-                inpt2 = input()
-        else:
-            print('А может быть все-таки введем целое число больше нуля?')
-            inpt2 = input()
-    print('Включать ли цифры?')
-    inpt3 = input()
-    while is_valid(inpt3) is False:
-        print('Не понимаю, попробуй еще раз')
-        inpt3 = input()
-    if is_valid(inpt3) == 'yeah':
-        chars += digits
-    print('Включать ли прописные буквы?')
-    inpt4 = input()
-    while is_valid(inpt4) is False:
-        print('Не понимаю, попробуй еще раз')
-        inpt4 = input()
-    if is_valid(inpt4) == 'yeah':
-        chars += uppercase_letters
-    print('Включать ли строчные буквы?')
-    inpt5 = input()
-    while is_valid(inpt5) is False:
-        print('Не понимаю, попробуй еще раз')
-        inpt5 = input()
-    if is_valid(inpt5) == 'yeah':
-        chars += lowercase_letters
-    print('Включать ли символы?')
-    inpt6 = input()
-    while is_valid(inpt6) is False:
-        print('Не понимаю, попробуй еще раз')
-        inpt6 = input()
-    if is_valid(inpt6) == 'yeah':
-        chars += punctuation
-    print('Исключать ли неоднозначные символы il1Lo0O?')
-    inpt7 = input()
-    while is_valid(inpt7) is False:
-        print('Не понимаю, попробуй еще раз')
-        inpt7 = input()
-    if is_valid(inpt7) == 'yeah':
-        for i in range(len(symb7)):
-            chars = chars.replace(symb7[i], '')
-    print('Ваши пароли:')
-    for i in range(inpt1):
-        print(generate_password(inpt2, chars))
-    print('')
-
-
-# Блок обработки запросов
-while True:
-    print('Версия программы 0.05 \n'
-          'Доступные команды: \n'
-          '1. Рассчитать net-pay из введенного gross-pay \n'
-          '2. Рассчитать gross-pay из введенного net-pay \n'
-          '3. Генератор безопасного пароля \n'
-          '0. Выход')
-    user_try = input()
-    if user_try == '1':
-        print('Введите gross-pay (только число) \n 0. Назад в главное меню')
-        pay = float(input())
-        if pay == 0:
-            continue
-        print(int(grosstonet(pay)))
-        continue
-    elif user_try == '2':
-        print('Введите net-pay (только число) \n'
-              '0. Назад в главное меню')
-        pay = float(input())
-        if pay == 0:
-            continue
-        print(nettogross(pay))
-        continue
-    elif user_try == '3':
-        password_generator()
-    elif user_try == '0':
-        print('Завершение работы')
-        break
-    else:
-        print('Неизвестная команда. Повторите ввод')
-        continue
